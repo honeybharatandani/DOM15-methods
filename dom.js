5069a503a2d7a787e4219f294f3ff869baed6e07
honeybharatandani <andanihoney106@gmail.com>
2021-05-11 15:26:19 +0530

    done

---
+++ dom.js
@@ -0,0 +1,79 @@
+function qs() {
+    document.querySelector("#qs").innerHTML = "How r u?";
+    document.querySelector(".qs").style.color = "blue";
+    document.querySelector(".qs").innerHTML = "BLUE color";
+}
+//********************************************************************************************************
+
+function qs2() { //used loop which selects all elements
+    var x, i;
+    x = document.querySelectorAll(".a");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.backgroundColor = "blue";
+    }
+}
+//*********************************************************************************************************
+document.addEventListener("mousemove", add);
+
+function add() {
+    document.getElementById("no").innerHTML = Math.random();
+}
+
+function removeHandler() {
+    document.removeEventListener("mousemove", add);
+}
+//---------------------------------------------------------------------------------------------------
+//function ce() {
+//var btn = document.createElement("button");
+// document.body.appendChild(btn);
+// }
+
+
+
+//************************************************************************************************************
+function ac() {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode("Mumbai");
+    node.appendChild(textnode);
+    document.getElementById("cities").appendChild(node);
+}
+
+//*******************************************************************
+
+function rc() {
+    var list = document.getElementById("city");
+    list.removeChild(list.childNodes[0]);
+}
+
+document.addEventListener("click", function() {
+        document.getElementById("mo").innerHTML = "You are Beautiful/Handsome !!!";
+    })
+    //****************************************************************************************************
+
+function replacechild() {
+    var elmnt = document.createElement("li");
+    var textnode = document.createTextNode("Water");
+    elmnt.appendChild(textnode);
+
+    var item = document.getElementById("myList");
+    item.replaceChild(elmnt, item.childNodes[0]);
+}
+
+function clone() {
+    var itm = document.getElementById("myList2").lastChild;
+    var cln = itm.cloneNode(true);
+    document.getElementById("myList1").appendChild(cln);
+}
+
+function set() {
+    document.getElementsByTagName("h3")[0].setAttribute("class", "democlass");
+}
+
+function insert() {
+    var newItem = document.createElement("LI");
+    var textnode = document.createTextNode("A.ngr");
+    newItem.appendChild(textnode);
+
+    var list = document.getElementById("in");
+    list.insertBefore(newItem, list.childNodes[0]);
+}
